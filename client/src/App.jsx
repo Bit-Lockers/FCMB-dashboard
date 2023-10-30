1fc7d6614031762d9b315e44abc4ffd03b3be075
Vector-9 <duzieblaise10@gmail.com>
2023-10-30 17:06:35 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,6 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Registration from "./pages/Registration/Registration";
-// import socketIO from "socket.io-client";
-
-// const socket = socketIO.connect("http://localhost:5000");
 
 function App() {
   return (
