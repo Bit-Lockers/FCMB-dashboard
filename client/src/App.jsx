b39456546c0537e03a6d3f981b7e6dba48a451ab
prechy123 <bamideleprecious100@gmail.com>
2023-10-30 18:38:19 +0100

    New loan request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Registration from "./pages/Registration/Registration";
 import Dashboard from "./pages/dashboard/Dashboard";
-import Setting from "./pages/setting/Setting";
+import NewLoanRequest from "./pages/myLoan/NewLoanRequest";
+// import Setting from "./pages/setting/Setting";
 // import VerticalTabs from "./pages/profile/Profile";
 // import MyLoanDashboard from "./pages/myLoan/MyLoanDashboard";
 // import LoanRequests from "./pages/loanRequests/LoanRequests";
@@ -15,7 +16,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Registration />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/loan" element={<Setting />} />
+      <Route path="/loan" element={<NewLoanRequest />} />
     </Routes>
   );
 }
