cfd02699fda87ccb2db57c6f995bcecbd271c302
prechy123 <bamideleprecious100@gmail.com>
2023-10-30 15:01:37 +0100

    setting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Registration from "./pages/Registration/Registration";
 import Dashboard from "./pages/dashboard/Dashboard";
-import VerticalTabs from "./pages/profile/Profile";
+import Setting from "./pages/setting/Setting";
+// import VerticalTabs from "./pages/profile/Profile";
 // import MyLoanDashboard from "./pages/myLoan/MyLoanDashboard";
 // import LoanRequests from "./pages/loanRequests/LoanRequests";
 // import SendMoney from "./pages/sendMoney/SendMoney";
@@ -14,7 +15,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Registration />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/loan" element={<VerticalTabs />} />
+      <Route path="/loan" element={<Setting />} />
     </Routes>
   );
 }
