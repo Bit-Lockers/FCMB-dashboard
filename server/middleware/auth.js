6b90f59f69359d8b4eb5ac3823ddd46d966960e1
Vector-9 <duzieblaise10@gmail.com>
2023-10-30 18:31:43 +0100

    middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,36 @@
+const User = require("../models/authModel");
+const jwt = require("jsonwebtoken");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("./catchAsyncErrors");
+
+// checks to see if user if authenticated or not to access a resource
+const authenticatedUser = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new ErrorHandler("Login first to access this resource.", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  console.log(token);
+  req.user = await User.findById(decoded.id);
+  // console.log(decoded.id);
+  next();
+});
+
+//Handling users roles
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role (${req.user.role}) is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
+
+module.exports = { authenticatedUser, authorizeRoles };
