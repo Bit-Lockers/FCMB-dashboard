1e0fbcfb1061a51b6683888a41441570a800f21d
prechy123 <bamideleprecious100@gmail.com>
2023-10-30 22:02:07 +0100

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Registration from "./pages/Registration/Registration";
 import Dashboard from "./pages/dashboard/Dashboard";
-import DisputeChatbot from "./pages/chatBot/DisputeChatBot";
+// import DisputeChatbot from "./pages/chatBot/DisputeChatBot";
 // import NewLoanRequest from "./pages/myLoan/NewLoanRequest";
+import LoanDetail from "./pages/loanRequestDetail/LoanDetail";
 // import ChatBot from "./pages/chatBot/ChatBot";
 // import Setting from "./pages/setting/Setting";
 // import VerticalTabs from "./pages/profile/Profile";
@@ -18,7 +19,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Registration />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/loan" element={<DisputeChatbot />} />
+      <Route path="/loan" element={<LoanDetail />} />
     </Routes>
   );
 }
